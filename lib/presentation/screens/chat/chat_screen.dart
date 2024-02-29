786daecb5b47202271f682acd7cbfaa2bff51ca6
Jade Martin <145301485+Jadusca@users.noreply.github.com>
2024-02-29 10:14:44 -0600

    Inicial

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://pyraxadon.files.wordpress.com/2021/09/screen-shot-2021-08-20-at-5.38.53-am.png?w=1200'),
+          ),
+        ),
+        title: const Text('Mi amorcito ❤️🥺'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                return (index % 2 == 0)
+                ? const HerMessageBubble()
+                : const MyMessageBubble();
+              })),
+
+              //caja de texto mesajes
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
